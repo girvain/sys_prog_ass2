89ae28d20592a9ae178aff953c8270b4e3fbb9dd
girvain <gavinross88@hotmail.co.uk>
2018-12-01 04:19:33 +0000

    updated code, stat stil won;t work

---
+++ .cquery_cached_index/@home@gavin@Dropbox@uni@systems_programing@ass2@cwk2_skeleton/client@client.c
@@ -0,0 +1,138 @@
+// Cwk2: client.c - message length headers with variable sized payloads
+//  also use of readn() and writen() implemented in separate code module
+#include "client_functions.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include "rdwrn.h"
+#include <sys/utsname.h>
+
+void displayOptions()
+{
+  puts("\nEnter an option:\n1. Display name/ID\n2. Display array of random ints\n3. Display the uname\n4. List files\n5. Get time\n6. Download file\n7. Exit\n");
+}
+
+void send_menu_option(int socket, char *hello_string)
+{
+  //char hello_string[32]; 
+    size_t n = strlen(hello_string) + 1;
+    writen(socket, (unsigned char *) &n, sizeof(size_t));	
+    writen(socket, (unsigned char *) hello_string, n);	  
+}
+
+void recieve_welcome_msg(int socket)
+{
+    char hello_string[32];
+    size_t k;
+
+    readn(socket, (unsigned char *) &k, sizeof(size_t));	
+    readn(socket, (unsigned char *) hello_string, k);
+
+    printf("Recieved menu option: %s\n", hello_string);
+    printf("Received: %zu bytes\n\n", k);
+} // end get_hello()
+
+int main(void)
+{
+    // *** this code down to the next "// ***" does not need to be changed except the port number
+    int sockfd = 0;
+    struct sockaddr_in serv_addr;
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+	perror("Error - could not create socket");
+	exit(EXIT_FAILURE);
+    }
+
+    serv_addr.sin_family = AF_INET;
+
+    // IP address and port of server we want to connect to
+    serv_addr.sin_port = htons(50001);
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // try to connect...
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1)  {
+	perror("Error - connect failed");
+	exit(1);
+    } else
+       printf("Connected to server...\n");
+
+    /** Use juse the file transfer */
+    //get_file2(sockfd);
+
+
+
+    
+    
+    //your own application code will go here and replace what is below... 
+    //i.e. your menu etc. /
+
+    int user_input;
+    while (user_input != 7)
+    {
+      displayOptions();
+        scanf("%i", &user_input);
+
+      //send_menu_option(sockfd, user_input);
+
+      switch(user_input) {
+      case 1 :
+       send_menu_option(sockfd, "1");
+        get_hello(sockfd);
+       //file_check(sockfd, "tt.txt");
+        break;
+      case 2 :
+        send_menu_option(sockfd, "2");
+        send_and_get_ints(sockfd);
+        break;
+      case 3 :
+        send_menu_option(sockfd, "3");
+        get_uts(sockfd);
+        break;
+      case 4 :
+        send_menu_option(sockfd, "4");
+        get_filenames(sockfd);
+        break;
+      case 5 :
+        send_menu_option(sockfd, "5");
+        get_time(sockfd);
+        break;
+      case 6 :
+        send_menu_option(sockfd, "6");
+        // send the name of file you want to the server
+        char fname[256] = "";
+        bzero(fname, 256); // clear the buffer
+        printf("Enter the file name you want \n");
+        scanf("%s", fname);
+        printf("This is whats in the buffer: %s\n", fname);
+
+        int condition = file_check(sockfd, fname);
+        printf("%d", condition);
+        if (condition == 0) {
+          //printf("file check exited right");
+          get_file2(sockfd, &fname);
+        }
+        
+        //get_file2(sockfd, &fname);
+       
+        break;
+      case 7 :
+        send_menu_option(sockfd, "7");
+        break;
+
+      }
+      //printf("Would you like another option[Y/n]\n");
+      }// end of while
+
+      
+    /*** make sure sockets are cleaned up */
+    close(sockfd);
+
+    exit(EXIT_SUCCESS);
+} // end main()
---
+++ .cquery_cached_index/@home@gavin@Dropbox@uni@systems_programing@ass2@cwk2_skeleton/client@client_functions.c
@@ -0,0 +1,247 @@
+#include "client_functions.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include "rdwrn.h"
+#include <sys/utsname.h>
+
+void get_hello(int socket)
+{
+    char hello_string[32];
+    size_t k;
+
+    readn(socket, (unsigned char *) &k, sizeof(size_t));	
+    readn(socket, (unsigned char *) hello_string, k);
+
+    printf("Hello String: %s\n", hello_string);
+    printf("Received: %zu bytes\n\n", k);
+} // end get_hello()
+
+void send_and_get_ints(int socket)  
+{
+  int array[5];
+  size_t payload_length;
+  size_t n = readn(socket, (unsigned char *) &payload_length, sizeof(size_t));
+  printf("payload_length is: %zu (%zu bytes)\n", payload_length, n);
+  n = readn(socket, (unsigned char *) array, payload_length);
+
+  // print the contents of the array
+  int i;
+  for (i = 0; i < 5; i++) {
+    printf("the array of ints: %i\n", array[i]);
+  }
+
+} 
+
+void get_random_numbers()
+{
+  char hello_string[32];
+  size_t k;
+
+  readn(socket, (unsigned char *) &k, sizeof(size_t));	
+  readn(socket, (unsigned char *) hello_string, k);
+}
+
+void get_uts(int socket)
+{
+  struct utsname uts;
+  size_t payload_length;
+  size_t n = readn(socket, (unsigned char *) &payload_length, sizeof(size_t));
+  printf("payload_length is: %zu (%zu bytes)\n", payload_length, n);
+  n = readn(socket, (unsigned char *) &uts, payload_length);
+
+  printf("Node name:    %s\n", uts.nodename);
+    printf("System name:  %s\n", uts.sysname);
+    printf("Release:      %s\n", uts.release);
+    printf("Version:      %s\n", uts.version);
+    printf("Machine:      %s\n\n", uts.machine);
+
+}
+
+/* Add an extra send/recieve to this function to get the size of the data being sent
+ * then make the hello_string that size.
+ */
+void get_filenames(int socket)
+{
+  char hello_string[512];
+    size_t k;
+   
+    readn(socket, (unsigned char *) &k, sizeof(size_t));
+    //char hello_string[k];
+    readn(socket, (unsigned char *) hello_string, k);
+
+    printf("filenames in upload directory:\n%s", hello_string);
+    //printf("Received: %zu bytes\n\n", k);
+
+    // payload way, not working with extra chars in results
+/* int array[256]; */
+/*   size_t payload_length; */
+/*   size_t n = readn(socket, (unsigned char *) &payload_length, sizeof(size_t)); */
+/*   printf("payload_length is: %zu (%zu bytes)\n", payload_length, n); */
+/*   n = readn(socket, (unsigned char *) array, payload_length); */
+
+/*  printf("filenames in upload directory:\n%s", array); */
+
+
+} // end get_hello()
+
+void get_time(int socket)
+{
+  char hello_string[32];
+    size_t k;
+
+    readn(socket, (unsigned char *) &k, sizeof(size_t));	
+    readn(socket, (unsigned char *) hello_string, k);
+    
+    printf("%s\n", hello_string);
+}
+
+/* ============================= recieve file ============================== */
+void gotoxy(int x,int y)
+ {
+ printf("%c[%d;%df",0x1B,y,x);
+ }
+
+void get_file(void *sockfd)
+{
+
+  int connfd = (int*)sockfd;
+  system("clear");
+  //int sockfd = 0;
+    int bytesReceived = 0;
+    char recvBuff[256];
+    memset(recvBuff, '0', sizeof(recvBuff));
+
+ /* Create file where data will be stored */
+  FILE *fp;
+  char fname[100] = "text.txt";
+  //read(sockfd, fname, 256);
+    //strcat(fname,"AK");
+    printf("File Name: %s\n",fname);
+    printf("Receiving file...");
+   	 fp = fopen(fname, "ab+");
+    	if(NULL == fp)
+    	{
+       	 printf("Error opening file");
+         return 1;
+    	}
+    long double sz=1;
+    /* Receive data in chunks of 256 bytes */
+    
+    while((bytesReceived = read(connfd, recvBuff, 256)) > 0)
+    {
+        sz++;
+        gotoxy(0,4);
+        printf("Received: %llf Mb\n",(sz/256));
+        fflush(stdout);
+        // recvBuff[n] = 0;
+        fwrite(recvBuff, sizeof(char),sizeof(recvBuff),fp);
+        printf("%s \n", recvBuff);
+    }
+
+    if(bytesReceived < 0)
+    {
+        printf("\n Read Error \n");
+    }
+    printf("\nFile OK....Completed\n");
+    //close(connfd);
+    return 0;
+}
+
+
+
+int get_file2(int sockfd, char* fname)
+{
+  //system("clear");
+    int bytesReceived = 0;
+    char recvBuff[1024];
+    memset(recvBuff, '0', sizeof(recvBuff));
+
+    // send the name of file you want to the server
+    /* char fname[256]; */
+    /*   printf("Enter the file name you want \n"); */
+    /*   scanf("%s", fname); */
+
+      
+      
+      size_t payload_length_fname = sizeof(fname);
+      writen(sockfd, (unsigned char *) &payload_length_fname, sizeof(size_t));
+      writen(sockfd, (unsigned char *) fname, payload_length_fname);
+
+
+      /* Create the file on the client side */ 
+    FILE *fp;
+	printf("File Name: %s\n",fname);
+	printf("Receiving file...");
+   	 fp = fopen(fname, "ab"); 
+    	if(NULL == fp)
+    	{
+       	 printf("Error opening file");
+         return 1;
+    	}
+    long double sz=1;
+    
+    // get the size of the file about to be sent 
+    int file_size;
+    size_t payload_length;
+    size_t n = readn(sockfd, (unsigned char *) &payload_length, sizeof(size_t));
+    n = readn(sockfd, (unsigned char *) recvBuff, payload_length);
+    file_size = atoi(recvBuff); // convert buffer data to int
+    printf("file size is %i\n", file_size);
+    int remain_data = file_size; // set this to the full size of the file
+    printf("%i", remain_data);
+        
+    /* Receive data in chunks of 1024 bytes */
+        //while(((bytesReceived = read(sockfd, recvBuff, 1024)) > 0) && (remain_data > 0))
+        while(remain_data > 0)
+    {
+      bytesReceived = read(sockfd, recvBuff, 1024);
+      sz++;
+      gotoxy(0,4);
+      printf("Received: %llf Mb",(sz/1024));
+      fflush(stdout);
+      // recvBuff[n] = 0;
+      fwrite(recvBuff, 1,bytesReceived,fp);
+      printf("%s \n", recvBuff);
+      printf("%i\n", bytesReceived);
+      remain_data -= bytesReceived;
+      bzero(recvBuff, 1024); // Clear the leftover stuff in the buffer 
+    }
+
+    if(bytesReceived < 0)
+    {
+        printf("\n Read Error \n");
+    }
+    printf("\nFile OK....Completed\n");
+    //close(sockfd);
+}
+
+
+int file_check(int socket, char fname[])
+{
+  size_t payload_length_fname = sizeof(fname);
+  writen(socket, (unsigned char *) &payload_length_fname, sizeof(size_t));
+  writen(socket, (unsigned char *) fname, payload_length_fname);
+  
+  char is_file[256];
+  size_t k;
+  readn(socket, (unsigned char *) &k, sizeof(size_t));
+  readn(socket, (unsigned char *) is_file, k);
+  
+    //printf(is_file);
+
+  if (strcmp(is_file, "File present\n") == 0) {
+    printf(is_file);
+    return 0;
+  }
+  printf(is_file);
+  return 1;
+  
+}
